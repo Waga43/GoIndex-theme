3d66677a9a001651994c3478ff6d8fbaa3023e2b
asparkoffire <kabir.47.chd@gmail.com>
2020-05-24 00:38:14 +0530

    update message
    Signed-off-by: asparkoffire <kabir.47.chd@gmail.com>

--- go2index/index.js
+++ go2index/index.js
@@ -1,25 +1,11 @@
 // =======Options START=======
 var authConfig = {
-  siteName: "GoIndex-theme-acrou", // 网站名称
-  version: "1.1.0", // 程序版本
+  siteName: "GoIndex-theme-acrou",
+  version: "1.1.0",
   theme: "acrou",
-  // 强烈推荐使用自己的 client_id 和 client_secret
   client_id: "202264815644.apps.googleusercontent.com",
   client_secret: "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
-  refresh_token: "", // 授权 token
-  /**
-   * 设置要显示的多个云端硬盘；按格式添加多个
-   * [id]: 可以是 团队盘id、子文件夹id、或者"root"（代表个人盘根目录）；
-   * [name]: 显示的名称
-   * [user]: Basic Auth 的用户名
-   * [pass]: Basic Auth 的密码
-   * [protect_file_link]: Basic Auth 是否用于保护文件链接，默认值（不设置时）为 false，即不保护文件链接（方便 直链下载/外部播放 等）
-   * 每个盘的 Basic Auth 都可以单独设置。Basic Auth 默认保护该盘下所有文件夹/子文件夹路径
-   * 【注意】默认不保护文件链接，这样可以方便 直链下载/外部播放;
-   *       如果要保护文件链接，需要将 protect_file_link 设置为 true，此时如果要进行外部播放等操作，需要将 host 替换为 user:pass@host 的 形式
-   * 不需要 Basic Auth 的盘，保持 user 和 pass 同时为空即可。（直接不设置也可以）
-   * 【注意】对于id设置为为子文件夹id的盘将不支持搜索功能（不影响其他盘）。
-   */
+  refresh_token: "",
   roots: [
     {
       id: "",
@@ -40,12 +26,6 @@ var authConfig = {
     },
   ],
   default_gd: 0,
-  /**
-   * 文件列表页面每页显示的数量。【推荐设置值为 100 到 1000 之间】；
-   * 如果设置大于1000，会导致请求 drive api 时出错；
-   * 如果设置的值过小，会导致文件列表页面滚动条增量加载（分页加载）失效；
-   * 此值的另一个作用是，如果目录内文件数大于此设置值（即需要多页展示的），将会对首次列目录结果进行缓存。
-   */
   files_list_page_size: 50,
   /**
    * 搜索结果页面每页显示的数量。【推荐设置值为 50 到 1000 之间】；
@@ -132,7 +112,7 @@ function html(current_drive_order = 0, model = {}) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
   <title>${authConfig.siteName}</title>
   <style>
-    @import url(https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@master/dist/style.min.css);
+    @import url(https://cdn.jsdelivr.net/gh/ASparkOfFire/GoIndex-theme@updates/dist/style.min.css);
   </style>
   <script>
     window.gdconfig = JSON.parse('${JSON.stringify({
@@ -149,7 +129,7 @@ function html(current_drive_order = 0, model = {}) {
 </head>
 <body>
     <div id="app"></div>
-    <script src="https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou@master/dist/app.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/ASparkOfFire/GoIndex-theme@updates/dist/app.min.js"></script>
 </body>
 </html>
 `;
